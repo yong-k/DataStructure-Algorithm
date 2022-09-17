2ec4d3337e51594cde45b237b03a541755a4fee4
jeongyong <cin03023@gmail.com>
2022-09-18 02:45:23 +0900

    binarySearch

---
+++ src/T013_binarySearch.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class T013_binarySearch {
+    static boolean binarySearch(ArrayList<Integer> list, Integer searchedItem) {
+        if (list.size() == 1 && list.get(0) == searchedItem)
+            return true;
+        if (list.size() == 1 && list.get(0) != searchedItem)
+            return false;
+        if (list.size() == 0)
+            return false;
+
+        int medium = list.size() / 2;
+
+        if (searchedItem == list.get(medium))
+            return true;
+        else {
+            if (searchedItem < list.get(medium))
+                return binarySearch(new ArrayList<Integer>(list.subList(0, medium)), searchedItem);
+            else
+                return binarySearch(new ArrayList<Integer>(list.subList(medium, list.size())), searchedItem);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++)
+            list.add((int)(Math.random() * 100));
+
+        Collections.sort(list);
+        System.out.println(binarySearch(list, 7));
+    }
+}
+
--- src/T999_practice.java
+++ src/T999_practice.java
@@ -1,54 +1,25 @@
 import java.util.ArrayList;
 
 public class T999_practice<T> {
-    static ArrayList<Integer> mergeSplitFunc(ArrayList<Integer> list) {
-        if (list.size() <= 1)
-            return list;
+    static boolean binarySearch(ArrayList<Integer> list, Integer searchedItem) {
+        if (list.size() ==1 && searchedItem == list.get(0))
+            return true;
+        if (list.size() == 1 && searchedItem != list.get(0))
+            return false;
+        if (list.size() == 0)
+            return false;
 
-        ArrayList<Integer> leftList, rightList;
         int medium = list.size() / 2;
 
-        leftList = mergeSplitFunc(new ArrayList<>(list.subList(0, medium)));
-        rightList = mergeSplitFunc(new ArrayList<>(list.subList(medium, list.size())));
+        if (searchedItem == list.get(medium))
+            return true;
+        else {
+            if (searchedItem < list.get(medium))
+                return binarySearch(new ArrayList<>(list.subList(0, medium)), searchedItem);
+            else
+                return binarySearch(new ArrayList<>(list.subList(medium, list.size())), searchedItem);
 
-        return mergeFunc(leftList, rightList);
-    }
-
-    static ArrayList<Integer> mergeFunc(ArrayList<Integer> leftList, ArrayList<Integer> rightList) {
-        ArrayList<Integer> mergeList = new ArrayList<>();
-        int leftPoint = 0, rightPoint = 0;
-
-        // CASE1: left/right 둘 다 있을 때
-        while (leftList.size() > leftPoint && rightList.size() > rightPoint) {
-            if (leftList.get(leftPoint) > rightList.get(rightPoint)) {
-                mergeList.add(rightList.get(rightPoint));
-                rightPoint++;
-            } else {
-                mergeList.add(leftList.get(leftPoint));
-                leftPoint++;
-            }
         }
-
-        // CASE2: right 데이터가 없을 때
-        while (leftList.size() > leftPoint) {
-            mergeList.add(leftList.get(leftPoint));
-            leftPoint++;
-        }
-
-        // CASE3: left 데이터가 없을 때
-        while (rightList.size() > rightPoint) {
-            mergeList.add(rightList.get(rightPoint));
-            rightPoint++;
-        }
-
-        return mergeList;
-    }
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 20; i++)
-            list.add((int)(Math.random()*100));
-
-        System.out.println(mergeSplitFunc(list));
     }
 }
 
