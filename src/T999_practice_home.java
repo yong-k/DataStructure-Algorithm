48f4d52fe8fe9352767fa3d37391603bb65d0e6b
jeongyong <cin03023@gmail.com>
2023-09-30 22:42:19 +0900

    230930_DataStructure2_solve ~~~ing,,,

--- src/T999_practice_home.java
+++ src/T999_practice_home.java
@@ -1,10 +1,9 @@
-package tony_git.data_structure2;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-public class B_2075 {
+
+public class T999_practice_home {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
